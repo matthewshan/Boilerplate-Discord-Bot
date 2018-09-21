fe7b5838d0ce126639c35e28c6713a6e611aff44
Eric Darr <darre@mail.gvsu.edu>
2018-09-20 22:30:51 -0400

    Added to readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Boilerplate-Discord-Bot
+
+# Requirements
+
+Python 3.6
+download here: https://www.python.org/downloads/release/python-366/
+
+On linux:
+```bash
+pip install discord
+```
+
+On Windows:
+pip install discord
+pip install aiohttp
+pip install websockets
+
+# How to run on Linux:
+
+```bash
+cd Boilerplate-Discord-Bot
+either
+./run.py
+or
+python3.6 run.py
+```
+
+
+```powershell
+cd Boilerplate-Discord-Bot
+python run.py
+```
+
+# Troubleshoot
+
+ - if you have issues running make sure you have your token.txt in the same directory
+ - If you have other issues ask on our discord
--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from sys import exit
 
 class Bot(discord.Client):
     def __init__(self):
@@ -6,16 +7,30 @@ class Bot(discord.Client):
 
     @staticmethod
     def getToken():
-        f = open('token.txt', 'r')
-        token = f.read().strip()
-        f.close()
+        try:
+            f = open('token.txt', 'r')
+            token = f.read().strip()
+            f.close()
+        except IOError:
+            print('A file named token.txt could not be found in this directory')
+            exit(1)
         return token
+
+    def fixHour(self, hour):
+        '''
+        the api gets utc time this converts it to est time and changes it from 24 hour time to 12 hour
+        '''
+        if(hour >= 4):
+            hour -= 4
+        else:
+            hour += 8
+        return hour % 12
 
     async def on_ready(self):
         print('Connected as:', self.user)
 
     async def on_typing(self, channel, user, when):
-        print('User: [{}] typing at time: {}:{}:{}'.format(user.display_name, when.hour-4, when.minute, when.second))
+        print('User: [{}] typing at time: {}:{}:{}'.format(user.display_name, self.fixHour(when.hour), when.minute, when.second))
 
     async def on_message(self, message):
         if(message.content.startswith('hello')):
